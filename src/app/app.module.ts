7b4ec01322d8fced11a052f49c2a00bf4e2a71b8
Homer Madriz <madriz_omar@hotmail.com>
2020-04-08 20:07:36 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductMainComponent } from '../Productos/product-main/product-main.component';
+import { HomeComponent } from '../home/home.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
+import { HeaderComponent } from '../header/header.component';
+import { ProductListComponent } from '../Productos/product-main/product-list/product-list.component';
+import { ProductComponent } from '../Productos/product-main/product-list/product/product.component';
+import { ProductDetailComponent } from '../Productos/product-main/product-detail/product-detail.component';
+import { ProductEditComponent } from '../Productos/product-main/product-edit/product-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductMainComponent,
+    HomeComponent,
+    NotFoundComponent,
+    HeaderComponent,
+    ProductListComponent,
+    ProductComponent,
+    ProductDetailComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule,
